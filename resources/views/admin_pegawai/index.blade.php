f81bc048065d3bd8ce23e8d5d324865dbdeff5a3
Clarissa Sheryl <clarissamaramis26@gmail.com>
2026-02-09 14:29:49 +0800

    refactor(pegawai): clean up admin table UI and consolidate actions

--- resources/views/admin_pegawai/index.blade.php
+++ resources/views/admin_pegawai/index.blade.php
@@ -1,5 +1,6 @@
 <h1>Data Pegawai</h1>
 
+{{-- FLASH MESSAGE --}}
 @if(session('success'))
     <p style="color: green">{{ session('success') }}</p>
 @endif
@@ -8,8 +9,14 @@
     <p style="color: red">{{ session('error') }}</p>
 @endif
 
-<table border="1" cellpadding="8">
-    <thead>
+<br>
+
+<a href="{{ route('pegawai.create') }}">+ Tambah Pegawai</a>
+
+<br><br>
+
+<table border="1" cellpadding="8" cellspacing="0" width="100%">
+    <thead style="background:#f5f5f5">
         <tr>
             <th>NIP</th>
             <th>Nama</th>
@@ -20,21 +27,30 @@
             <th>Aksi</th>
         </tr>
     </thead>
+
     <tbody>
-        @foreach($pegawai as $p)
+        @forelse($pegawai as $p)
         <tr>
             <td>{{ $p->nip }}</td>
             <td>{{ $p->nama }}</td>
             <td>{{ $p->unit_kerja }}</td>
             <td>{{ $p->no_whatsapp }}</td>
-            <td>{{ $p->status }}</td>
+
+            {{-- STATUS PEGAWAI --}}
+            <td>
+                @if($p->status === 'aktif')
+                    <span style="color: green; font-weight: bold;">Aktif</span>
+                @else
+                    <span style="color: gray;">Nonaktif</span>
+                @endif
+            </td>
 
             {{-- STATUS AKUN LOGIN --}}
             <td>
                 @if($p->user)
                     <span style="color: green;">Akun Aktif</span>
                 @else
-                    <span style="color: rgb(255, 30, 0);">Belum Ada</span>
+                    <span style="color: red;">Belum Ada</span>
                 @endif
             </td>
 
@@ -48,28 +64,31 @@
                         Buat Akun Login
                     </a>
                 @endif
+
+                |
+                <form action="{{ route('pegawai.destroy', $p->id_pegawai) }}"
+                      method="POST"
+                      style="display:inline"
+                      onsubmit="return confirm('Yakin ingin menonaktifkan pegawai ini?')">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit"
+                        style="
+                            background:none;
+                            border:none;
+                            color:red;
+                            cursor:pointer;
+                            padding:0;
+                        ">
+                        Nonaktifkan
+                    </button>
+                </form>
             </td>
         </tr>
-        @endforeach
+        @empty
+        <tr>
+            <td colspan="7" align="center">Belum ada data pegawai</td>
+        </tr>
+        @endforelse
     </tbody>
 </table>
-
-<br>
-<a href="{{ route('pegawai.create') }}">+ Tambah Pegawai</a>
-<ul>
-@foreach($pegawai as $p)
-    <li>
-        {{ $p->nama }} - {{ $p->unit_kerja }}
-        <a href="{{ route('pegawai.edit', $p->id_pegawai) }}">Edit</a>
-    </li>
-    <form action="{{ route('pegawai.destroy', $p->id_pegawai) }}" 
-      method="POST" 
-      style="display:inline;"
-      onsubmit="return confirm('Yakin ingin menonaktifkan pegawai ini?')">
-    @csrf
-    @method('DELETE')
-    <button type="submit">Nonaktifkan</button>
-</form>
-@endforeach
-</ul>
-
